55c625940236ed0a590f748ff7a2c06a04b645a6
SodiqIshola <ishola_o_sodiq@yahoo.com>
2026-04-20 10:53:23 -0400

    update the applicationset argocd and application env

---
+++ terraform/aws-eks/environments/application/variables.tf
@@ -0,0 +1,11 @@
+variable "appset_dir" {
+  description = "Path to the directory containing ArgoCD manifests"
+  type        = string
+  default     = "./k8s/apps/nodejs-app/argo-cd/appset"
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "ca-central-1" 
+  description = "The AWS region where all infrastructure will be deployed"
+}
